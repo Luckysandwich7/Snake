cc1dc9efd9f4bc94e7ca5b2e18ae49e35309b232
Luckysandwich7 <90877920+Luckysandwich7@users.noreply.github.com>
2022-03-17 19:48:51 -0600

    Add files via upload

---
+++ credit.py
@@ -0,0 +1,8 @@
+class Credit:
+
+    def __init__(self):
+        self._name = "Davi Ferreira do Vale, \nInnocent Hove, \nRyan Alvord, \n& Njato Harizo "
+        pass
+
+    def getCredits(self):
+        return "Updated by: "+ self._name
